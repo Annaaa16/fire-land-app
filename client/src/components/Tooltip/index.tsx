381851249d41d68a35a12819cca92bd9c57ba3d9
minhhung <minhhunghuynh1106@gmail.com>
2021-09-16 18:12:38 +0700

    Update tooltip classes 🍖

--- client/src/components/Tooltip/index.tsx
+++ client/src/components/Tooltip/index.tsx
@@ -3,19 +3,26 @@ import clsx from 'clsx';
 
 interface IProps {
   title: string;
+  direction: string;
 }
 
 function Tooltip(props: IProps) {
-  const { title } = props;
+  const { title, direction } = props;
+  console.log(direction);
 
   return (
     <div
       className={clsx(
-        'absolute top-1/2 left-full',
-        'i-flex-center text-2xs font-bold transform -translate-y-1/2 translate-x-3.5 py-1.25 px-3 rounded-3xl shadow-2xl opacity-0 invisible',
+        'absolute',
+        direction === 'rtl' ? 'top-1/2 left-full' : '-top-9 -left-0',
+        'i-flex-center text-2xs font-bold min-w-max transform py-1.25 px-3 rounded-3xl shadow-2xl opacity-0 invisible',
+        direction === 'rtl' ? '-translate-y-1/2 translate-x-3.5' : '',
         'text-white bg-dk-tooltip-hv',
         'transition-all duration-250',
-        'group-hover:translate-x-2 group-hover:opacity-100 group-hover:visible'
+        'group-hover:opacity-100 group-hover:visible',
+        direction === 'rtl'
+          ? 'group-hover:translate-x-2'
+          : 'group-hover:translate-y-2'
       )}>
       <span>{title}</span>
     </div>
