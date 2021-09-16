27ee947adf791903819720668ff7e7c5f60300c8
minhhung <minhhunghuynh1106@gmail.com>
2021-09-16 18:13:07 +0700

    Done newsfeed members 🍖

---
+++ client/src/features/NewsFeed/components/NewsFeedMembers/index.tsx
@@ -0,0 +1,46 @@
+// clsx
+import clsx from 'clsx';
+
+// material ui icons
+import GroupIcon from '@material-ui/icons/Group';
+import ChatIcon from '@material-ui/icons/Chat';
+import Tooltip from '@/components/Tooltip';
+
+function NewsFeedMembers() {
+  return (
+    <div
+      className={clsx(
+        'flex items-center justify-between mt-7 p-4 shadow-md rounded-xl',
+        'bg-lt-cpn dark:bg-dk-cpn'
+      )}>
+      <div className={clsx('flex items-center')}>
+        <div
+          className={clsx(
+            'i-flex-center w-11 h-11 mr-3 rounded-full',
+            'bg-lt-newsfeed-group dark:bg-primary-v4'
+          )}>
+          <GroupIcon className={clsx('text-primary-v1 dark:text-white')} />
+        </div>
+        <span className={clsx('text-dk-gray dark:text-dk-text')}>
+          Total members:
+        </span>
+        <span className={clsx('ml-1 font-bold', 'dark:text-primary-v4')}>
+          08
+        </span>
+      </div>
+
+      <div
+        className={clsx(
+          'relative',
+          'i-flex-center group w-11 h-11 mr-2 rounded-full',
+          'bg-lt-newsfeed-group dark:bg-primary-v4',
+          'cursor-pointer'
+        )}>
+        <ChatIcon className={clsx('text-primary-v1 dark:text-white')} />
+        <Tooltip title='Go to chat room!' direction='ttb' />
+      </div>
+    </div>
+  );
+}
+
+export default NewsFeedMembers;
--- client/src/features/NewsFeed/index.tsx
+++ client/src/features/NewsFeed/index.tsx
@@ -6,6 +6,7 @@ import Header from '@/components/Header';
 import Sidebar from '@/components/Sidebar';
 import SidebarSmall from '@/components/SidebarSmall';
 import NewsFeedBanner from './components/NewsFeedBanner';
+import NewsFeedMembers from './components/NewsFeedMembers';
 
 function NewsFeed() {
   return (
@@ -17,6 +18,7 @@ function NewsFeed() {
       <main
         className={clsx('lg:w-newsfeed-content-w px-4 lg:px-0 mx-auto py-10')}>
         <NewsFeedBanner />
+        <NewsFeedMembers />
       </main>
     </Meta>
   );
