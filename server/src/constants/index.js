a9f068e4e5367998dec40c2f435d7333eed0e099
minhhung <minhhunghuynh1106@gmail.com>
2021-09-21 09:22:01 +0700

    Done auth server 🍒

---
+++ server/src/constants/index.js
@@ -0,0 +1,7 @@
+exports.LIMIT_UPLOAD = '5mb';
+exports.ACCESS_TOKEN_LIFE = 30000;
+
+exports.MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@fire-chat-app-global-se.y50eq.mongodb.net/fire-chat-app-global-server?retryWrites=true&w=majority`;
+
+exports.ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+exports.REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const { ACCESS_TOKEN_SECRET } = require('../constants');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, message: 'Access token not found' });
+  }
+
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (error, user) => {
+    if (error) {
+      return res.status(403).json({ success: false, message: 'Invalid token' });
+    }
+
+    // Attach userId to request after decoded user param
+    req.userId = user.id;
+
+    next();
+  });
+};
+
+module.exports = verifyToken;
