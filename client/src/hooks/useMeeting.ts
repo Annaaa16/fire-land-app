60c864ed42e594f966458ce7596298ce58ba1942
minhhung <minhhunghuynh1106@gmail.com>
2021-09-30 17:28:27 +0700

    Add custom hook 🍑

---
+++ client/src/hooks/useMeeting.ts
@@ -0,0 +1,28 @@
+import { MutableRefObject, useEffect, useState } from 'react';
+
+const useMeeting = (
+  ref: MutableRefObject<HTMLElement | null>,
+  rootMargin = '0px'
+) => {
+  const [isMeeting, setIsMeeting] = useState(false);
+
+  useEffect(() => {
+    const options = {
+      rootMargin,
+    };
+
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsMeeting(entry.isIntersecting);
+    }, options);
+
+    const { current } = ref;
+
+    if (current) {
+      observer.observe(current);
+    }
+  }, []);
+
+  return isMeeting;
+};
+
+export default useMeeting;
