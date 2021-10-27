70bf7d674231f9106db2079a60c3e185369d7edd
igdev116 <minhhunghuynh1106@gmail.com>
2021-10-27 14:20:54 +0700

    Update cloudinary prefix name  🌥️

--- client/src/utils/images.ts
+++ client/src/utils/images.ts
@@ -1,12 +1,14 @@
-import { CLOUDINARY_PREFIX } from '@/constants';
+import { URLS } from '@/constants';
+
+const { CLOUDINARY } = URLS;
 
 // Common
-export const ltBackground = `${CLOUDINARY_PREFIX}/v1631503820/fire-chat-app/common/gmtca69qmkg1jqv9qmkf.jpg`;
-export const dkBackground = `${CLOUDINARY_PREFIX}/v1631544025/fire-chat-app/common/jwmgicokwtgtw0dwhmtk.jpg`;
-export const rocket = `${CLOUDINARY_PREFIX}/v1631503853/fire-chat-app/common/qttqsjesgtwux6vwdi2p.png`;
-export const ltDot = `${CLOUDINARY_PREFIX}/v1631535065/fire-chat-app/common/c553kaaote9gle2ixceb.png`;
-export const dkDot = `${CLOUDINARY_PREFIX}/v1631546575/fire-chat-app/common/uackw0a5ke9kncdtfz9u.png`;
+export const ltBackground = `${CLOUDINARY}/v1631503820/fire-chat-app/common/gmtca69qmkg1jqv9qmkf.jpg`;
+export const dkBackground = `${CLOUDINARY}/v1631544025/fire-chat-app/common/jwmgicokwtgtw0dwhmtk.jpg`;
+export const rocket = `${CLOUDINARY}/v1631503853/fire-chat-app/common/qttqsjesgtwux6vwdi2p.png`;
+export const ltDot = `${CLOUDINARY}/v1631535065/fire-chat-app/common/c553kaaote9gle2ixceb.png`;
+export const dkDot = `${CLOUDINARY}/v1631546575/fire-chat-app/common/uackw0a5ke9kncdtfz9u.png`;
 
 // Newsfeed
-export const bannerShape = `${CLOUDINARY_PREFIX}/v1631780448/fire-chat-app/newsfeed/banner/p4smcip89spsavygmbry.png`;
-export const bannerMembers = `${CLOUDINARY_PREFIX}/v1631780422/fire-chat-app/newsfeed/banner/owa4exzdanqribyacbmf.png`;
+export const bannerShape = `${CLOUDINARY}/v1631780448/fire-chat-app/newsfeed/banner/p4smcip89spsavygmbry.png`;
+export const bannerMembers = `${CLOUDINARY}/v1631780422/fire-chat-app/newsfeed/banner/owa4exzdanqribyacbmf.png`;
