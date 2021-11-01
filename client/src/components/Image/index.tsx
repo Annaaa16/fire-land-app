82572ead6e74a74bb5ea2a3cb32777fa91c1539e
igdev116 <minhhunghuynh1106@gmail.com>
2021-11-01 22:30:24 +0700

    Add image component 👘

--- client/src/components/Image/index.tsx
+++ client/src/components/Image/index.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { default as NextImage } from 'next/image';
 
+// nanoid
+import { nanoid } from 'nanoid';
+
 // material ui icons
 import MovieOutlinedIcon from '@mui/icons-material/MovieOutlined';
 
@@ -10,19 +13,52 @@ import clsx from 'clsx';
 // types
 import { ImageProps } from 'next/image';
 
-function Image(props: ImageProps) {
-  const { src, ...rest } = props;
+function Image(
+  props: ImageProps & { widths?: number[]; height?: number; subClass?: string }
+) {
+  const { src, widths, height, subClass, ...rest } = props;
 
   const [isError, setIsError] = useState<boolean>(false);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   return (
     <>
-      {isError ? (
+      {!isError ? (
+        <NextImage
+          src={src}
+          {...rest}
+          onError={() => setIsError(true)}
+          onLoad={() => setIsLoaded(false)}
+          onLoadingComplete={() => setIsLoaded(true)}
+          className={clsx(
+            isLoaded && !isError
+              ? 'opacity-100 visible'
+              : 'opacity-0 invisible',
+            subClass
+          )}
+        />
+      ) : (
         <div className={clsx('i-flex-center w-full h-full')}>
           <MovieOutlinedIcon className={clsx('!text-4xl', 'text-gray')} />
         </div>
-      ) : (
-        <NextImage src={src} {...rest} onError={() => setIsError(true)} />
+      )}
+      {!isLoaded && !isError && (
+        <div className='h-full w-full'>
+          <div className='animate-pulse flex space-x-4'>
+            <div className='flex-1 space-y-2 py-1'>
+              {(widths || [85, 20, 30, 40, 65]).map((width) => (
+                <div
+                  key={nanoid(6)}
+                  className={clsx('rounded', 'bg-gray-700')}
+                  style={{
+                    width: width + '%',
+                    height: height ? height + 'px' : '16px',
+                  }}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       )}
     </>
   );
