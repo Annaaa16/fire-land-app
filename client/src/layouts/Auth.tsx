5e9fb022de31134682e6604a8601a255fc3d2f6e
igdev116 <minhhunghuynh1106@gmail.com>
2021-11-15 22:43:50 +0700

    Create auth layout for login and register 📖

---
+++ client/src/layouts/Auth.tsx
@@ -0,0 +1,125 @@
+// clsx
+import clsx from 'clsx';
+
+// types
+import { ReactNode } from 'react';
+
+import { LOCAL_STORAGE } from '@/constants';
+import { useGlobalContext } from '@/contexts/GlobalContext';
+
+import Meta from '@/layouts/Meta';
+
+// images
+import {
+  ltBackground,
+  dkBackground,
+  rocket,
+  ltDot,
+  dkDot,
+} from '@/utils/images';
+
+// svgs
+import logo from '@/assets/svgs/logo.svg';
+
+interface AuthProps {
+  title: string;
+  question: string;
+  recommend: string;
+  children: ReactNode;
+  onRedirect: () => void;
+}
+
+function Auth(props: AuthProps) {
+  const { title, question, recommend, children, onRedirect } = props;
+
+  const { theme } = useGlobalContext();
+
+  return (
+    <Meta title='Login'>
+      <div
+        className={clsx(
+          'relative md:overflow-hidden',
+          'grid grid-cols-1 lg:grid-cols-2 w-screen h-full lg:h-screen py-16 px-2.5 md:py-50 lg:py-0 lg:px-50',
+          'lg:bg-center bg-cover bg-no-repeat'
+        )}
+        style={{
+          backgroundImage: `url(${
+            theme === LOCAL_STORAGE.DARK_THEME_VALUE
+              ? dkBackground
+              : ltBackground
+          })`,
+        }}>
+        <div
+          className={clsx('i-flex-center flex-col text-center mb-8 lg:mb-0')}>
+          <h3
+            className={clsx(
+              'font-bold text-xl lg:text-2xl mb-5',
+              'text-white'
+            )}>
+            WELCOME TO
+          </h3>
+          <img
+            src={logo.src}
+            alt='Logo'
+            loading='lazy'
+            className={clsx('w-36 mb-2', 'cursor-pointer select-none')}
+          />
+          <p className='hidden lg:block text-white mt-2 leading-6 text-sm px-10'>
+            The next generation social network & community! Connect with your
+            friends and play with our quests and badges gamification system!
+          </p>
+        </div>
+
+        <div
+          style={{
+            backgroundImage: `url(${
+              theme === LOCAL_STORAGE.DARK_THEME_VALUE ? dkDot : ltDot
+            })`,
+          }}
+          className={clsx(
+            'absolute right-[-32%] top-[-20%]',
+            'hidden lg:block rounded-full h-[140%] w-[64%]',
+            'bg-lt-cpn dark:bg-dk-cpn'
+          )}
+        />
+
+        <div
+          className={clsx(
+            'relative',
+            'i-flex-center flex-col w-full md:w-[424px] lg:w-[484px] lg:h-[625px] mr-auto lg:mr-0 ml-auto lg:my-auto pt-12 pb-6 md:pb-8 px-5 md:px-10 lg:px-16 lg:py-16 rounded-lg shadow-lg',
+            'bg-lt-cpn dark:bg-dk-cpn'
+          )}>
+          <img
+            src={rocket}
+            className={clsx(
+              'absolute -left-20 -top-16',
+              'hidden lg:block w-30'
+            )}
+            alt='Label'
+          />
+          <h3 className={clsx('font-bold text-2xl', 'dark:text-white')}>
+            {title}
+          </h3>
+
+          <p className={clsx('text-xs mt-8 leading-6', 'dark:text-white')}>
+            {question} {''}
+            <span
+              onClick={onRedirect}
+              className={clsx(
+                'font-bold',
+                'text-primary-v2 dark:text-primary-v4',
+                'cursor-pointer'
+              )}>
+              {recommend}
+            </span>
+            !
+          </p>
+
+          {children}
+        </div>
+      </div>
+    </Meta>
+  );
+}
+
+export default Auth;
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,33 +1,17 @@
-// clsx
-import clsx from 'clsx';
-
-import { PATHS, LOCAL_STORAGE } from '@/constants';
+import { PATHS } from '@/constants';
 import { authActions } from '@/redux/slices/authSlice';
-import { useGlobalContext } from '@/contexts/GlobalContext';
 import { useAuthSelector } from '@/redux/selectors';
 import { useEffect } from 'react';
 import { useRouter } from 'next/dist/client/router';
 import useStoreDispatch from '@/hooks/useStoreDispatch';
 
 import Meta from '@/layouts/Meta';
-import LandingLeft from '@/components/LandingLeft';
 import LoginForm from '@/features/Login/components/LoginForm';
 import LoginSocial from '@/features/Login/components/LoginSocial';
 
-import {
-  ltBackground,
-  dkBackground,
-  rocket,
-  ltDot,
-  dkDot,
-} from '@/utils/images';
-
-// styles
-import styles from '@/styles/common.module.scss';
+import Auth from '@/layouts/Auth';
 
 function Login() {
-  const { theme } = useGlobalContext();
-
   const router = useRouter();
   const dispatch = useStoreDispatch();
 
@@ -37,7 +21,7 @@ function Login() {
 
   const moveToRegister = () => {
     dispatch(authActions.clearMessage());
-    router.push('/register');
+    router.push(PATHS.REGISTER);
   };
 
   useEffect(() => {
@@ -48,70 +32,14 @@ function Login() {
 
   return (
     <Meta title='Login'>
-      <div
-        className={clsx(
-          'relative md:overflow-hidden',
-          'grid grid-cols-1 lg:grid-cols-2 w-screen h-full lg:h-screen py-16 px-2.5 md:py-50 lg:py-0 lg:px-50',
-          'lg:bg-center bg-cover bg-no-repeat'
-        )}
-        style={{
-          backgroundImage: `url(${
-            theme === LOCAL_STORAGE.DARK_THEME_VALUE
-              ? dkBackground
-              : ltBackground
-          })`,
-        }}>
-        <LandingLeft />
-
-        <div
-          style={{
-            backgroundImage: `url(${
-              theme === LOCAL_STORAGE.DARK_THEME_VALUE ? dkDot : ltDot
-            })`,
-          }}
-          className={clsx(
-            'hidden lg:block',
-            'bg-lt-cpn dark:bg-dk-cpn',
-            styles.decoration
-          )}
-        />
-
-        <div
-          className={clsx(
-            'relative',
-            'i-flex-center flex-col w-full md:w-[424px] lg:w-[484px] lg:h-[625px] mr-auto lg:mr-0 ml-auto lg:my-auto pt-12 pb-6 md:pb-8 px-5 md:px-10 lg:px-16 lg:py-16 rounded-lg shadow-lg',
-            'bg-lt-cpn dark:bg-dk-cpn'
-          )}>
-          <img
-            src={rocket}
-            className={clsx(
-              'absolute -left-20 -top-16',
-              'hidden lg:block w-30'
-            )}
-            alt='Label'
-          />
-          <h3 className={clsx('font-bold text-2xl', 'dark:text-white')}>
-            Account Login
-          </h3>
-
-          <p className={clsx('text-xs mt-8 leading-6', 'dark:text-white')}>
-            Don't have an account? {''}
-            <span
-              onClick={moveToRegister}
-              className={clsx(
-                'font-bold',
-                'text-primary-v2 dark:text-primary-v4',
-                'cursor-pointer'
-              )}>
-              Create new Account
-            </span>
-            !
-          </p>
-
-          <LoginForm />
-          <LoginSocial />
-        </div>
-      </div>
+      <Auth
+        title='Account Login'
+        question="Don't have an account?"
+        recommend='Create new Account'
+        onRedirect={moveToRegister}>
+        <LoginForm />
+        <LoginSocial />
+      </Auth>
     </Meta>
   );
 }
