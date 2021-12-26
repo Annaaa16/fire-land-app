eeecc446298cf5b4139e751b1da90784798f8c13
igdev <minhhunghuynh1106@gmail.com>
2021-12-26 21:22:23 +0700

    Fix loading spacing 📙

--- client/src/components/Loading/LoadingPost.tsx
+++ client/src/components/Loading/LoadingPost.tsx
@@ -18,21 +18,16 @@ function LoadingPost() {
 
   return (
     <div className={clsx('w-full h-80 mt-7 rounded-lg p-4 shadow-md')}>
-      <div className={clsx('flex items-center mb-5')}>
-        <Skeleton className='w-1/4' />
-        <div className={clsx('flex items-center space-x-2')}>
-          <Skeleton className='w-7' />
-          <Skeleton className='w-7' />
-        </div>
+      <div className={clsx('flex space-x-2 items-center mb-5')}>
+        <Skeleton className='w-1/6' />
+        <Skeleton className='w-1/12' />
       </div>
-      <div className={clsx('flex items-center space-x-3')}>
+      <div className={clsx('flex items-center space-x-3 mb-5')}>
         <Skeleton className='w-1/5' />
         <Skeleton className='w-2/12' />
-        <Skeleton className='w-1/12' />
-        <Skeleton className='w-5/12' />
+        <Skeleton className='w-4/12' />
         <Skeleton className='w-1/12' />
       </div>
-      <div className={clsx('flex items-center space-x-3')}></div>
       <div className={clsx('flex items-center space-x-3')}>
         <Skeleton className='w-1/12' />
         <Skeleton className='w-1/12' />
