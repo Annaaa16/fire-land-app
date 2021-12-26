18c080c8feeff13fe9c60243cb3b0a8ec0e146b3
igdev <minhhunghuynh1106@gmail.com>
2021-12-26 21:23:21 +0700

    Fix react key 📙

--- client/src/components/Post/PostCommentList.tsx
+++ client/src/components/Post/PostCommentList.tsx
@@ -1,6 +1,3 @@
-// nanoid
-import { nanoid } from 'nanoid';
-
 import { useCommentsSelector } from '@/redux/selectors';
 import PostComment from './PostComment';
 
@@ -23,8 +20,8 @@ function PostCommentList(props: PostCommentListProps) {
     <>
       {filteredComments?.length > 0 && isOpenComments && (
         <div className='mt-3'>
-          {filteredComments.map((comment) => (
-            <PostComment key={nanoid(6)} comment={comment} />
+          {filteredComments.map((comment, idx) => (
+            <PostComment key={'post-comment' + idx} comment={comment} />
           ))}
         </div>
       )}
