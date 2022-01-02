77a36ddc78343a9193261cfb70ba4d149a47b32a
igdev <minhhunghuynh1106@gmail.com>
2022-01-02 14:13:57 +0700

    Add condition for queries middleware 🚧

--- server/src/middlewares/queriesMiddleware.js
+++ server/src/middlewares/queriesMiddleware.js
@@ -1,6 +1,12 @@
 const verifyQueries = (req, res, next) => {
   const { page, limit } = req.query;
 
+  if (!page && !limit) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Page and limit queries is required' });
+  }
+
   if (!page) {
     return res
       .status(400)
